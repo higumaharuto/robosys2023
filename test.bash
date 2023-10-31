0dfa7c540cdf775ed567cc85bfabe164a330e4dd
Haruto Higuma <tongxiongyouren@gmail.com>
2023-10-31 22:33:09 +0900

    add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Haruto Higuma
+# SPDX-License-Indentifier: BSD-3Clause
+
+ng () {
+	echo NG at Line $1
+	ret=1
+}
+
+res=0
+
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
