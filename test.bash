5e9dee05416512fed7208e40ce6cfa2ecb53a410
Haruto Higuma <tongxiongyouren@gmail.com>
2023-11-01 02:37:40 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Haruto Higuma
 # SPDX-License-Indentifier: BSD-3Clause
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST (入出力のテスト)###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT (変な入力を入れるテスト)###
+out=$(echo あ | ./plus)
+[ "$?" = 1]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字のテスト
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
