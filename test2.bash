9b8b472a750316632926796b7c4c6d515388bf1a
Haruto Higuma <tongxiongyouren@gmail.com>
2023-11-25 22:11:36 +0900

    test update

--- test2.bash
+++ test2.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2023 Haruto Higuma
 # SPDX-License-Indentifier: BSD-3-Clause
 
@@ -12,6 +12,14 @@ res=0
 out=$(seq 15 | ./plus)
 
 [ "$(echo "${out}" | tr -d '\n')" = "120-120ans1は100以上の数ですans2は100以下の数です" ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　
+out=$(echo | ./plus) 
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK 
 exit $res
