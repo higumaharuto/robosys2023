7d1f60e7f4172efe00a14ba98511c54889bf5728
Haruto Higuma <tongxiongyouren@gmail.com>
2023-11-25 21:33:16 +0900

    test update

--- test2.bash
+++ test2.bash
@@ -2,7 +2,17 @@
 # SPDX-FileCopyrightText: 2023 Haruto Higuma
 # SPDX-License-Indentifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
+
 out=$(seq 15 | ./plus)
 
-[ "$(echo "${out}" | tr -d '\n')" = "120-120ans1は100以上の数ですans2は100以下の数です" ]
+[ "$(echo "${out}" | tr -d '\n')" = "120-120ans1は100以上の数ですans2は100以下の数です" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK 
+exit $res
 
